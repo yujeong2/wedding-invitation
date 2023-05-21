e63c1eb1b735faead093ccecd4fe53452d1f1a45
yujeong2 <chyj971008@gmail.com>
2023-05-22 00:50:25 +0900

    googlesheet 추가 / dependency 수정

---
+++ src/assets/keys/index.js
@@ -0,0 +1,2 @@
+export const GITHUB_LINK = 'https://yujeong2.github.io/wedding-invitation/';
+export const KAKAO_KEY = '065f3e15b5876a16f51d03fa86509539';
---
+++ src/hooks/useGoogleSheet.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-alert, no-new, import/no-extraneous-dependencies */
+
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { useEffect, useState } from 'react';
+import credential from '../assets/keys/googleSheetCredential.json';
+
+// 구글 시트 조회하는 로직
+export const getGoogleSheet = async () => {
+  const doc = new GoogleSpreadsheet(
+    '1r4plvRjuGHmotVICPWXUzbKeKEbXAWuopRPbeEDlHq4',
+  );
+  // 구글 인증이 필요하다.
+  await doc.useServiceAccountAuth(credential);
+  await doc.loadInfo();
+  return doc;
+};
+
+// 구글 시트 조회하는 custom useHook
+const useGoogleSheet = (sheetId) => {
+  const [googleSheetRows, setGoogleSheetRows] = useState([]);
+
+  const fetchGoogleSheetRows = async () => {
+    const googleSheet = await getGoogleSheet();
+    const sheetsByIdElement = googleSheet.sheetsById[sheetId];
+    const rows = await sheetsByIdElement.getRows();
+    setGoogleSheetRows(rows);
+  };
+
+  useEffect(() => {
+    fetchGoogleSheetRows();
+  }, []);
+
+  return [googleSheetRows];
+};
+
+export default useGoogleSheet;
