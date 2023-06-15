ed7e5f3811b18e1e9843a1bd942e8fe0ff949e90
yujeong2 <chyj971008@gmail.com>
2023-06-15 23:50:13 +0900

    오류수정

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -103,6 +103,7 @@ export default function Quiz({ setQuizModal }) {
       setData((curData) => {
         const newData = { ...curData };
         newData.score = curData.score + 10;
+        return newData;
       });
     }
     setStatus('result');
